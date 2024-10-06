97ada968da8573942d849d58053d92bfd19ca5e9
Luan Martins <moondev94@outlook.com>
2024-10-06 03:38:49 -0300

    feat: creates StyledButton components

--- src/components/StyledButton/StyledButton.tsx
+++ src/components/StyledButton/StyledButton.tsx
@@ -1,22 +1,37 @@
 import { styled } from "@mui/material"
+import { ReactNode } from "react"
 
 
+interface StyledButtonProps {
+    children: ReactNode
+}
 
-const StyledButton = () => {
+
+const StyledButton: React.FC<StyledButtonProps> = ({children}) => {
 
     const StyledButton = styled("button")(({theme}) => ({
         backgroundColor: "transparent",
-        border: '1px solid white',
+        border: `1px solid ${theme.palette.primary.light}`,
         borderRadius: "3px",
         padding: "5px 15px",
-        width: "100%"
+        width: "100%",
+        color: theme.palette.primary.contrastText,
+        display: "inline-flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "10px",
+        '&: hover': {
+            backgroundColor: theme.palette.secondary.light
+        }
 
        
     }))
 
     return (
       <>
-        <StyledButton>texto</StyledButton>
+        <StyledButton>
+            {children}
+        </StyledButton>
       </>
     )
   }
